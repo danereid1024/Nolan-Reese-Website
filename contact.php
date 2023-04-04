334a10657200285cb8e1dbfbd9b4ab4f1bedf475
danereid1024 <102497454+danereid1024@users.noreply.github.com>
2023-04-03 21:15:43 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,9 +1,16 @@
 <?php 
+
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
+    $name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
     $subject = $_POST['subject'];
     $mailFrom = $_POST['mail'];
+    if (!filter_var($mailFromm, FILTER_VALIDATE_EMAIL)) {
+        $error_msg = "Please enter a valid email address.";
+
+    }
     $message = $_POST['message'];
+
 
     $mailTo = "danereid1024@icloud.com";
     $headers = "From: ".$mailFrom;
